41fd8d59b954dd0df2b452680208bb7a46cb1f70
KeisukeShimokawa <shimoke4869@gmail.com>
2021-01-20 01:24:53 +0900

    docs: 画像やスクリプト読み込み、XHRやFetchでのCookieを実験 #19

---
+++ web_basics/05_Third_Party_Cookie/experiment-cookie/app.js
@@ -0,0 +1,13 @@
+const appFirst = require("./app-first");
+const appThird = require("./app-third");
+
+const PORT_FIRST = process.env.PORT_FIRST || 8080;
+const PORT_THIRD = process.env.PORT_THIRD || 8090;
+
+appFirst.listen(PORT_FIRST, () => {
+  console.log(`first-site listening on ${PORT_FIRST}`);
+})
+
+appThird.listen(PORT_THIRD, () => {
+  console.log(`third-site listening on ${PORT_THIRD}`);
+})
