2fa5bf5da0cc6d28f63acb5c8c7cdb3a0b227b71
KeisukeShimokawa <shimoke4869@gmail.com>
2021-01-27 15:14:30 +0000

    docs: TypeScriptをコンパイル後のJavaScriptを削除 #30

--- web_basics/06_CORS/cors/dist/middleware/error.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, req, res, next) => {
-    res.status(500).json({
-        message: err.message,
-    });
-};
-exports.default = errorHandler;
