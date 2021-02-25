394b0682a87eb05244b6b05437867ce4770b25a4
KeisukeShimokawa <shimoke4869@gmail.com>
2021-02-25 03:48:58 +0000

    feat: Storybookに関する設定を追加 #54

--- test_basics/11_Storybook/webpack.config.js
+++ test_basics/11_Storybook/webpack.config.js
@@ -1,20 +1,23 @@
-import path from "path";
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
 
 module.exports = {
   // process.env.NODE_ENVに"development"を設定する
-  mode: "development",
+  mode: 'development',
+
+  devtool: 'inline-source-map',
 
   // エントリポイントのファイルを設定する
   entry: {
-    bundle: "./src/index.tsx",
+    bundle: './src/index.tsx',
   },
 
   // バンドルしたファイルの出力先を決定する
   output: {
     // 出力先フォルダ名
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
     // 出力先ファイル名
-    filename: "bundled.js",
+    filename: 'bundled.js',
   },
 
   // デフォルトではwebpackはJSかJSONしか読み込めない
@@ -25,9 +28,13 @@ module.exports = {
         // TypeScriptの拡張子の場合
         test: /\.tsx?$/,
         // TypeScriptを以下でコンパイル
-        use: "ts-loader",
+        use: 'ts-loader',
         // 以下のフォルダは除外
         exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
@@ -35,13 +42,13 @@ module.exports = {
   // import File from '../path/to/file';した際にTypeScriptを読み込む
   // これで読み込む際に拡張子を指定する必要がなくなる
   resolve: {
-    extensions: [".ts", ".tsx", ".js", "json"],
+    extensions: ['.ts', '.tsx', '.js', 'json'],
   },
 
   // webpack-dev-server用の設定
   devServer: {
     // 開発サーバを起動する起点を設定する
-    contentBase: path.resolve(__dirname, "public"),
+    contentBase: path.resolve(__dirname, 'public'),
     // gzip圧縮を使用する
     compress: true,
     // リクエストを受け付けるポート番号を指定する
