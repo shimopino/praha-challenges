8b3ab2fe1a7ef142e2be2cd5a293c0d971a4e069
KeisukeShimokawa <shimoke4869@gmail.com>
2021-03-03 23:02:28 +0900

    fix: create-react-appで作り直し

--- test_basics/TicTacToe/src/container/Page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Game } from '../components/Game/Game';
-import { useTicTacToe } from '../hooks/useTicTacToe';
-
-export const Page = (): JSX.Element => {
-  const [status, current, history, handleClick, jumpTo] = useTicTacToe();
-
-  return <Game {...{ status, current, history, handleClick, jumpTo }} />;
-};
--- test_basics/TicTacToe/src/types/tictactoe.ts
+++
@@ -1,5 +0,0 @@
-export type SquareValue = 'X' | 'O' | null;
-
-export interface BoardValue {
-  squares: SquareValue[];
-}
