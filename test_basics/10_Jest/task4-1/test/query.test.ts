14d1f9c89e983e53801e3c32083d95c95fd8788d
kamimi01 <awakaru0122@gmail.com>
2021-03-07 19:43:41 +0900

    feat: クイズ1に回答

--- test_basics/10_Jest/task4-1/test/query.test.ts
+++ test_basics/10_Jest/task4-1/test/query.test.ts
@@ -1,5 +1,112 @@
-import {parseQuery} from '../src/query';
+import {parseQuery, SearchQuery} from '../src/query';
 
 describe('query test suite', () => {
-  test.todo('テストを実装してみましょう。');
+  // test.todo('テストを実装してみましょう。');
+  test('インプット：test', () => {
+    const testData = 'test';
+    const expected: SearchQuery = {
+      keywords: {
+        positive: ['test'],
+        negative: [],
+      },
+      phrases: {
+        positive: [],
+        negative: [],
+      },
+    };
+
+    const received = parseQuery(testData);
+
+    expect(received).toEqual(expected);
+  });
+
+  test('インプット：-test', () => {
+    const testData = '-test';
+    const expected: SearchQuery = {
+      keywords: {
+        positive: [],
+        negative: ['test'],
+      },
+      phrases: {
+        positive: [],
+        negative: [],
+      },
+    };
+
+    const received = parseQuery(testData);
+
+    expect(received).toEqual(expected);
+  });
+
+  test('インプット："test"', () => {
+    const testData = '"test"';
+    const expected: SearchQuery = {
+      keywords: {
+        positive: [],
+        negative: [],
+      },
+      phrases: {
+        positive: ['test'],
+        negative: [],
+      },
+    };
+
+    const received = parseQuery(testData);
+
+    expect(received).toEqual(expected);
+  });
+
+  test('インプット：-"test"', () => {
+    const testData = '-"test"';
+    const expected: SearchQuery = {
+      keywords: {
+        positive: [],
+        negative: [],
+      },
+      phrases: {
+        positive: [],
+        negative: ['test'],
+      },
+    };
+
+    const received = parseQuery(testData);
+
+    expect(received).toEqual(expected);
+  });
+
+  test('インプット：--"test"', () => {
+    const testData = '--"test"';
+    const expected: SearchQuery = {
+      keywords: {
+        positive: [],
+        negative: [],
+      },
+      phrases: {
+        positive: [],
+        negative: ['test'],
+      },
+    };
+
+    const received = parseQuery(testData);
+
+    expect(received).toEqual(expected);
+  });
+
+  test('インプット：testa -testb "testc" -"testd" --"teste"', () => {
+    const testData = 'testa -testb "testc" -"testd" --"teste"';
+    const expected: SearchQuery = {
+      keywords: {
+        positive: ['testa'],
+        negative: ['testb'],
+      },
+      phrases: {
+        positive: ['testc'],
+        negative: ['testd', 'teste'],
+      },
+    };
+
+    const received = parseQuery(testData);
+
+    expect(received).toEqual(expected);
+  });
 });
