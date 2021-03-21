0b4c219788eac56bd74f840e62fc3c60675da397
KeisukeShimokawa <shimoke4869@gmail.com>
2021-03-21 06:20:09 +0000

    feat: [Green] 掛け算のTODOリストの5番目 例外メッセージの条件と文章を変更 #64

--- test_basics/15_TDD/tdd-practice/src/script.ts
+++ test_basics/15_TDD/tdd-practice/src/script.ts
@@ -1,8 +1,8 @@
 const MAX_ARGS_LENGTH = 30;
 
 export const multiply = (...args: number[]): number => {
-  if (args.length > MAX_ARGS_LENGTH) {
-    throw new Error('引数の数は30個以内にしてください');
+  if (args.length < 1 || args.length > MAX_ARGS_LENGTH) {
+    throw new Error('引数の数は1個以上30個以内にしてください');
   }
 
   return args.reduce((previousValue: number, currentValue: number): number => {
