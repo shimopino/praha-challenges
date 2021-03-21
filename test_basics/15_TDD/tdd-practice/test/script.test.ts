3f964ea6f28096fcc723d9be436adaf467821621
KeisukeShimokawa <shimoke4869@gmail.com>
2021-03-21 05:56:26 +0000

    feat: 掛け算のTODOリストの4番目をレッド状態で追加 #64

--- test_basics/15_TDD/tdd-practice/test/script.test.ts
+++ test_basics/15_TDD/tdd-practice/test/script.test.ts
@@ -57,7 +57,46 @@ describe('TDDの練習', () => {
       // Assert
       expect(actual).toBe(expected);
     });
-    it.todo('引数を31個まで受け取ってエラーが発生する');
+    it('引数を31個まで受け取ってエラーが発生する', () => {
+      // Arrange
+      const args: number[] = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20,
+        21,
+        22,
+        23,
+        24,
+        25,
+        26,
+        27,
+        28,
+        29,
+        30,
+        31,
+      ];
+      // Act & Assert
+      expect(functions.multiply(...args)).toThrow(
+        new Error('引数の数は30個以内にしてください'),
+      );
+    });
     it.todo('引数を渡さなかった場合にエラーが発生する');
     it.todo('引数が数字以外の場合にエラーが発生する');
     it.todo('計算結果が1000を超える場合に「big big number」を表示する');
