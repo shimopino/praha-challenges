6a950e641351e058e6a2bee7a9959fe81d19afbd
KeisukeShimokawa <shimoke4869@gmail.com>
2021-03-21 05:51:23 +0000

    feat: 掛け算のTODOリストの3番目をレッド状態で追加 #64

--- test_basics/15_TDD/tdd-practice/test/script.test.ts
+++ test_basics/15_TDD/tdd-practice/test/script.test.ts
@@ -53,7 +53,50 @@ describe('TDDの練習', () => {
       // Assert
       expect(actual).toBe(expected);
     });
-    it.todo('引数を30個まで受け取ってその掛け算を返す');
+    it('引数を30個まで受け取ってその掛け算を返す', () => {
+      // Arrange
+      const args: number[] = [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20,
+        21,
+        22,
+        23,
+        24,
+        25,
+        26,
+        27,
+        28,
+        29,
+        30,
+      ];
+      const expected = args.reduce(
+        (previousValue: number, currentValue: number): number => {
+          return previousValue * currentValue;
+        },
+      );
+      // Act
+      const actual = functions.multiply(...args);
+      // Assert
+      expect(actual).toBe(expected);
+    });
     it.todo('引数を31個まで受け取ってエラーが発生する');
     it.todo('引数を渡さなかった場合にエラーが発生する');
     it.todo('引数が数字以外の場合にエラーが発生する');
