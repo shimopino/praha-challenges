5987c0d7eaf6c14d3e9b4d7edce4a8960dae2a34
KeisukeShimokawa <shimoke4869@gmail.com>
2021-03-21 06:33:35 +0000

    feat: [Green] big big numberを送出するような実装を追加 #64

--- test_basics/15_TDD/tdd-practice/src/script.ts
+++ test_basics/15_TDD/tdd-practice/src/script.ts
@@ -1,11 +1,22 @@
+const MIN_ARGS_LENGTH = 1;
 const MAX_ARGS_LENGTH = 30;
 
 export const multiply = (...args: number[]): number => {
-  if (args.length < 1 || args.length > MAX_ARGS_LENGTH) {
+  const MAX_RESULT = 1000;
+
+  if (args.length < MIN_ARGS_LENGTH || args.length > MAX_ARGS_LENGTH) {
     throw new Error('引数の数は1個以上30個以内にしてください');
   }
 
-  return args.reduce((previousValue: number, currentValue: number): number => {
-    return previousValue * currentValue;
-  });
+  const result = args.reduce(
+    (previousValue: number, currentValue: number): number => {
+      return previousValue * currentValue;
+    },
+  );
+
+  if (result > MAX_RESULT) {
+    throw new Error('big big number');
+  }
+
+  return result;
 };
