95cdad957592cef46ef34428cb47eed6fdbb740e
KeisukeShimokawa <shimoke4869@gmail.com>
2021-03-22 14:30:01 +0000

    feat: [Red] TODOリスト1つ目をRed状態で追加 #64

--- test_basics/15_TDD/tdd-practice/test/add.test.ts
+++ test_basics/15_TDD/tdd-practice/test/add.test.ts
@@ -1,7 +1,17 @@
 import * as functions from '../src/script';
 
 describe('掛け算を行う add メソッドのTDD', () => {
-  it.todo('引数に1と2を渡して3が返る');
+  it('引数に1と2を渡して3が返る', () => {
+    // Arrange
+    const arg1 = 1;
+    const arg2 = 2;
+    const expected = 3;
+    // Act
+    const actual = functions.add(arg1, arg2);
+    // Assert
+    expect(actual).toBe(expected);
+  });
+
   it.todo('引数を30個まで受け取り、その合計値を返す');
   it.todo('引数を31個まで受け取った場合にエラーが発生する');
   it.todo('引数を渡さなかった場合にエラーが発生する');
