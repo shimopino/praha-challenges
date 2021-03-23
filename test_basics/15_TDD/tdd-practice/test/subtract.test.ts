c27aa4277afec32a9f29b379993856a92fbe2c92
KeisukeShimokawa <shimoke4869@gmail.com>
2021-03-23 11:26:58 +0000

    feat: 引き算メソッドのTODOリストを追加 #64

---
+++ test_basics/15_TDD/tdd-practice/test/subtract.test.ts
@@ -0,0 +1,13 @@
+import * as functions from '../src/script';
+import { ErrorMessage } from '../src/enums';
+
+describe('引き算を行う subtract メソッドのTDD', () => {
+  it.todo('引数に3と2を渡して1を返す');
+  it.todo('引数を30個まで受け取ることができる');
+  it.todo('引数を31個まで受け取りエラーが発生する');
+  it.todo('引数を渡さなかった場合にエラーが発生する');
+  it.todo('引数が数字以外の場合にエラーが発生する');
+  it.todo('計算結果がマイナスの場合に「negative number」を表示する');
+  it.todo('計算結果が0の場合にそのまま結果を返す');
+  it.todo('計算結果が1の場合にそのまま結果を返す');
+});
