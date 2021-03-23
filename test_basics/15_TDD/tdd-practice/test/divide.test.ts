63d120dd1a88cfef099c598e94730df45fb4e992
KeisukeShimokawa <shimoke4869@gmail.com>
2021-03-23 12:24:53 +0000

    feat: 割り算のメソッド divide のTODOリストを追加 #64

---
+++ test_basics/15_TDD/tdd-practice/test/divide.test.ts
@@ -0,0 +1,12 @@
+import * as functions from '../src/script';
+import {} from '../src/enums';
+
+describe('割り算を行う divide メソッドのTDD', () => {
+  it.todo('引数に4と2を渡して2が返る');
+  it.todo('引数に5と2を渡して1.5が返る（有限小数）');
+  it.todo('引数に1と3を渡して0.333が返る（無限小数）');
+  it.todo('引数を30個まで受け取ることができる');
+  it.todo('引数を31個まで受け取った場合にエラーが発生する');
+  it.todo('引数を渡さなかった場合にエラーが発生する');
+  it.todo('引数が数字以外の場合にエラーが発生する');
+});
