096310bb29f5728bf299fae2befbf81fadffc6ac
KeisukeShimokawa <shimoke4869@gmail.com>
2021-08-11 22:29:06 +0900

    feat: 不要なファイルを削除する #116

--- architecture/36_DDD/task_2/src/app.module.ts
+++ architecture/36_DDD/task_2/src/app.module.ts
@@ -1,14 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PostService } from './post.service';
-import { PrismaService } from './prisma.service';
-import { SampleController } from './sample.controller';
-import { UserService } from './user.sservice';
 
 @Module({
   imports: [],
-  controllers: [AppController, SampleController],
-  providers: [AppService, PrismaService, UserService, PostService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
