5415d8ae7b52adb98768d4b5297ba6111d403771
KeisukeShimokawa <shimoke4869@gmail.com>
2021-08-17 08:48:44 +0900

    feat: テストに関する設定を追加 #116

---
+++ architecture/36_DDD/prisma-tutorial/aggregation/jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+  },
+  roots: ['<rootDir>'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+};
