4ff506324f863f163e0217a10dab7a9b303cba20
KeisukeShimokawa <shimoke4869@gmail.com>
2021-08-18 23:34:54 +0900

    feat: テスト毎にPrismaのDB接続を遮断する設定に変更 #116

--- architecture/36_DDD/prisma-tutorial/aggregation/src/__tests__/service.spec.ts
+++ architecture/36_DDD/prisma-tutorial/aggregation/src/__tests__/service.spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { Prisma, PrismaClient } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { GetPostService } from 'src/post/service';
 
@@ -19,11 +18,12 @@ describe('GetPostService', () => {
   afterEach(async () => {
     await prisma.truncate();
     await prisma.resetSequences();
-  });
-
-  afterAll(async () => {
     await prisma.$disconnect();
   });
+
+  // afterAll(async () => {
+  //   await prisma.$disconnect();
+  // });
 
   it('Service should not be undefined', () => {
     expect(service).toBeDefined();
@@ -42,87 +42,3 @@ describe('GetPostService', () => {
     expect(true).toBeTruthy();
   });
 });
-
-export const toCamelCase = (text: string): string => {
-  return text
-    .replace(/(?:^\w|[A-Z]|\b\w)/g, (leftTrim: string, index: number) =>
-      index === 0 ? leftTrim.toLowerCase() : leftTrim.toUpperCase(),
-    )
-    .replace(/\s+/g, '');
-};
-
-const buildPrismaInclude = (attrs: Record<string, any>) => {
-  const include = Object.keys(attrs).reduce((prev, curr) => {
-    const value = attrs[curr];
-    const isObject = typeof value === 'object';
-
-    const isRelation =
-      isObject && Object.keys(value).find((v) => v.match(/connect|craete/));
-
-    if (isRelation) {
-      prev[curr] = true;
-    }
-
-    return prev;
-  }, Object.create(null));
-
-  const hasInclude = Object.keys(include).length;
-  return hasInclude ? include : undefined;
-};
-
-const createFactory = <T>(params: {
-  modelName: string;
-  defaultAttrs?: Partial<T>;
-}) => {
-  const prisma = new PrismaClient();
-
-  const { modelName, defaultAttrs } = params;
-
-  const FactoryFunctions = {
-    build: (attrs: Partial<T> = {}) => {
-      return {
-        ...defaultAttrs,
-        ...attrs,
-      } as T;
-    },
-
-    create: async (attrs: Partial<T> = {}) => {
-      const data = FactoryFunctions.build(attrs);
-      const prismaOptions: Record<string, any> = {};
-      const includes = buildPrismaInclude(attrs);
-
-      if (includes) prismaOptions.include = includes;
-
-      const prismaModel = toCamelCase(modelName as unknown as string);
-
-      return await prisma[prismaModel].create({
-        data,
-        ...prismaOptions,
-      });
-    },
-  };
-
-  return FactoryFunctions;
-};
-
-const client = new PrismaClient();
-console.dir(client, { depth: 1 });
-
-console.log(toCamelCase('UserModel'));
-console.log(Prisma.dmmf.datamodel.models);
-console.log(Prisma.dmmf.datamodel.models.map((model) => model.name));
-
-// const modelNames = Prisma.dmmf.datamodel.models.map((model) => model.name);
-// // const modelNamesTypes = modelNames as const;
-// // console.log(modelNames);
-// type Models = typeof modelNames[number];
-
-// createFactory<Prisma.UserCreateInput>({
-//   modelName: 'Users',
-// });
-
-const userfactory = createFactory<Prisma.PostCreateInput>({
-  modelName: 'Post',
-  defaultAttrs: { title: '1', contents: '2' },
-});
-userfactory.create({ Author: { create: { name: 'user1', email: 'email1' } } });
