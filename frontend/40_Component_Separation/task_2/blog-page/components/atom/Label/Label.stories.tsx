1a3b0dc4988ff22e825e6bc650e64bc6817658ec
KeisukeShimokawa <shimoke4869@gmail.com>
2021-10-31 20:58:36 +0900

    ラベルコンポーネントの配置場所を変更 #120

--- frontend/40_Component_Separation/task_2/blog-page/components/atom/Label/Label.stories.tsx
+++ frontend/40_Component_Separation/task_2/blog-page/components/atom/Label/Label.stories.tsx
@@ -2,7 +2,7 @@ import { Label } from "./Label";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 
 export default {
-  title: "Example/Label",
+  title: "Atom/Label",
   component: Label,
 } as ComponentMeta<typeof Label>;
 
@@ -13,5 +13,23 @@ const Template: ComponentStory<typeof Label> = (args) => {
 export const Laravel = Template.bind({});
 Laravel.args = {
   text: "Laravel",
+  href: "#",
+};
+
+export const Design = Template.bind({});
+Design.args = {
+  text: "Design",
+  href: "#",
+};
+
+export const PHP = Template.bind({});
+PHP.args = {
+  text: "PHP",
+  href: "#",
+};
+
+export const Testing = Template.bind({});
+Testing.args = {
+  text: "Testing",
   href: "#",
 };
