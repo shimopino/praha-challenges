90ab9339cba94c82e849e5310ea07ec68d29da88
KeisukeShimokawa <shimoke4869@gmail.com>
2021-11-07 16:34:16 +0900

    feat: 対象コンポーネントの描画設定を追加 #120

--- frontend/40_Component_Separation/task_2/blog-page/pages/index.tsx
+++ frontend/40_Component_Separation/task_2/blog-page/pages/index.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from "next";
+import { Icon } from "../components/atom/Icon/Icon";
 import { Label } from "../components/atom/Label/Label";
+import { Link } from "../components/atom/Link/Link";
 
 const Home: NextPage = () => {
   return (
     <>
       <Label text="Laravel" />
+      <br />
+      <Link
+        text={"ReadMore"}
+        href={"https://tailwindcomponents.com/component/blog-page/landing"}
+      />
+      <br />
+      <Icon
+        src={
+          "https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=731&q=80"
+        }
+        alt={"avatar"}
+      />
     </>
   );
 };
