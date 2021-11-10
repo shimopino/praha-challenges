1a660e7d3c6d38792c0298f1ce77b366561e2570
KeisukeShimokawa <shimoke4869@gmail.com>
2021-11-11 05:20:01 +0900

    feat: フォルダ構成を変更 #155

--- tutorial/nestjs/auth/src/users/users.service.ts
+++ tutorial/nestjs/auth/src/users/users.service.ts
@@ -1,24 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
-// This should be a real class/interface representing a user entity
-export type User = any;
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+};
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
+  private readonly users: User[] = [
     {
-      userId: 1,
-      username: 'john',
+      id: 1,
+      name: 'john',
+      email: 'john@example.com',
       password: 'changeme',
     },
     {
-      userId: 2,
-      username: 'maria',
+      id: 2,
+      name: 'maria',
+      email: 'maria@example.com',
       password: 'guess',
     },
   ];
 
   async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.username === username);
+    return this.users.find((user) => user.name === username);
   }
 }
