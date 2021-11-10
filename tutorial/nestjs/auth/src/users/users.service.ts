3af5ec75f5daa3e87a71c97fd8f2da7cb8867c39
KeisukeShimokawa <shimoke4869@gmail.com>
2021-11-11 05:42:42 +0900

    feat: ユーザーをDBに登録・検索するための処理を追加 #155

--- tutorial/nestjs/auth/src/users/users.service.ts
+++ tutorial/nestjs/auth/src/users/users.service.ts
@@ -1,30 +1,40 @@
-import { Injectable } from '@nestjs/common';
-
-export type User = {
-  id: number;
-  name: string;
-  email: string;
-  password: string;
-};
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { nanoid } from 'nanoid';
+import { PrismaService } from 'src/utils/prisma.service';
+import { CreateUserDTO } from './dtos/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  private readonly users: User[] = [
-    {
-      id: 1,
-      name: 'john',
-      email: 'john@example.com',
-      password: 'changeme',
-    },
-    {
-      id: 2,
-      name: 'maria',
-      email: 'maria@example.com',
-      password: 'guess',
-    },
-  ];
+  constructor(private prisma: PrismaService) {}
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.name === username);
+  async register(userData: CreateUserDTO) {
+    const user = await this.prisma.user.create({
+      data: {
+        id: nanoid(),
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+      },
+    });
+
+    delete user.password;
+
+    return user;
+  }
+
+  async findOne(email: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException(
+        'メールアドレスに該当するユーザーは存在しません。',
+      );
+    }
+
+    return user;
   }
 }
