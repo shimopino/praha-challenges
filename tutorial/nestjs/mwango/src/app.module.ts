70097ea1d3792fe4cfc93336c28a088aaefe2aa6
KeisukeShimokawa <shimoke4869@gmail.com>
2021-11-14 22:05:26 +0900

    feat: リフレッシュトークンの事前設定を追加 #155

--- tutorial/nestjs/mwango/src/app.module.ts
+++ tutorial/nestjs/mwango/src/app.module.ts
@@ -12,8 +12,10 @@ import * as Joi from 'joi';
     AuthModule,
     ConfigModule.forRoot({
       validationSchema: Joi.object({
-        JWT_SECRET: Joi.string().required(),
-        JWT_EXPIRATION_TIME: Joi.string().required(),
+        JWT_ACCESS_TOKEN_SECRET: Joi.string().required(),
+        JWT_ACCESS_TOKEN_EXPIRATION_TIME: Joi.string().required(),
+        JWT_REFRESH_TOKEN_SECRET: Joi.string().required(),
+        JWT_REFRESH_TOKEN_EXPIRATION_TIME: Joi.string().required(),
       }),
     }),
   ],
